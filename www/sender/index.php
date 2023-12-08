8eba0c3f51cc0456d9f25e58c6062d80756c17d9
Vadim Podolyan <vadim.podolyan@deonlinedrogist.nl>
2023-12-08 12:13:58 +0200

    clear code

--- www/sender/index.php
+++ www/sender/index.php
@@ -12,37 +12,14 @@ $channel->queue_declare('hello', false, true, false, false);
 
 $lines = file('./files/newfile.txt');
 foreach ($lines as $line_num => $line) {
-    /*$myfile = fopen("./files/result.txt", "a") or die("Unable to open file!");
-    fwrite($myfile, $line);
-    fclose($myfile);*/
     $msg = new AMQPMessage($line,
    // array('delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT)
     );
     $channel->basic_publish($msg, '', 'hello');
     echo "Line #<b>{$line_num}</b> : " . htmlspecialchars($line) . "<br />\n";
 }
-/*
-$msg = new AMQPMessage('1111111',
-   // array('delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT)
-);
 
-$msg1 = new AMQPMessage('222222222222222!',
-   /// array('delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT)
-);
-$msg2 = new AMQPMessage('3333333333333!',
-   /// array('delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT)
-);
-*/
 //$channel->basic_publish($msg, '', 'hello');
-
-echo " [x] Sent 'Hello World New! 123'\n";
 
 $channel->close();
 $connection->close();
-/*
-$myfile = fopen("/var/www/sender/files/newfile.txt", "w+") or die("Unable to open file!");
-$txt = "John Doe\n";
-fwrite($myfile, $txt);
-$txt = "Jane Doe\n";
-fwrite($myfile, $txt);
-fclose($myfile);*/
