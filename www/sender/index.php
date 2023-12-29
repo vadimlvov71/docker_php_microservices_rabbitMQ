3b8da521eca2147b1d7e8fdb91cea4a2b9f63f6a
Vadim Podolyan <vadim.podolyan@deonlinedrogist.nl>
2023-12-29 19:49:46 +0200

    name spacing and OOP fixing

--- www/sender/index.php
+++ www/sender/index.php
@@ -1,39 +1,49 @@
 <?php
+namespace Index;
+
 error_reporting(E_ALL ^ E_DEPRECATED);
 require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/service/RabbitEnv.php';
 
-use PhpAmqpLib\Connection\AMQPStreamConnection;
+use App\Classes\Rabbit;
+use App\Service\RabbitEnv;
 use PhpAmqpLib\Message\AMQPMessage;
 
-$connection = new AMQPStreamConnection(
-    RabbitEnv::Host->value, 
-    RabbitEnv::Port->value, 
-    RabbitEnv::Username->value, 
-    RabbitEnv::getPassword()
-);
-$channel = $connection->channel();
+/**
+ * rabbitMQ script
+ */
+class Application {
+    /**
+     * run a rabbitMQ script
+     * @return void
+     */
+    public function run(): void
+    {
+        $rabbit_connect = new Rabbit();
+       
+        $connection = $rabbit_connect->getConnection();
+        
+        $channel = $connection->channel();
+        $channel->exchange_declare(
+            RabbitEnv::Exchange->value, 
+            'fanout', # type
+            false,    # passive
+            false,    # durable
+            false     # auto_delete
+        );
 
-$channel->exchange_declare(
-    RabbitEnv::Exchange->value, 
-    'fanout', # type
-    false,    # passive
-    false,    # durable
-    false     # auto_delete
-);
-
-$lines = file('./files/newfile.txt');
-
-foreach ($lines as $line_num => $line) {
-    $msg = new AMQPMessage($line,
-   // array('delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT)
-    );
-    $channel->basic_publish($msg, RabbitEnv::Exchange->value,);
-    echo "Line #<b>{$line_num}</b> : " . htmlspecialchars($line) . "<br />\n";
-    echo round(memory_get_usage() / 1024 / 1024, 2) . ' MB' . PHP_EOL;
+        $lines = file('./files/newfile.txt');
+        foreach ($lines as $line_num => $line) {
+            $msg = new AMQPMessage($line,
+           // array('delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT)
+            );
+            $channel->basic_publish($msg, RabbitEnv::Exchange->value,);
+            $rabbit_connect->setMessage($line_num, $line);
+        }
+        
+        $channel->close();
+        $connection->close();
+    }
 }
-
-$channel->close();
-$connection->close();
-
+$application = new Application();
+$application->run();
 
